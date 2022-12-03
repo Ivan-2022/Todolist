dca54365dc9d73e8a99cc52d594c6dc062586b49
352 <ivan-2022>
2022-12-03 19:06:48 +0300

    add docker-ci-env7

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -22,7 +22,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-environ.Env.read_env(os.path.join(BASE_DIR / '.env'))
+environ.Env.read_env(os.path.join('.env'))
 # environ.Env.read_env()
 
 
