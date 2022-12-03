24e7eb0285f015754e52814fe047e21b2f488dc5
352 <ivan-2022>
2022-12-03 14:31:23 +0300

    add docker-ci-env

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -22,23 +22,23 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-# environ.Env.read_env(os.path.join(BASE_DIR / '././.env'))
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR / '././.env'))
+# environ.Env.read_env()
 
 
-def get_env_variable(var_name):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "set the %s environment variable" % var_name
-        raise ImproperlyConfigured(error_msg)
+# def get_env_variable(var_name):
+#     try:
+#         return os.environ[var_name]
+#     except KeyError:
+#         error_msg = "set the %s environment variable" % var_name
+#         raise ImproperlyConfigured(error_msg)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = get_env_variable("${secrets.SECRET_KEY}")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
