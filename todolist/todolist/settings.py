aa8fd26ec442826c9e291e8b403e0fb7426cd7a6
352 <ivan-2022>
2022-12-03 08:06:49 +0300

    add env_path settings

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -21,7 +21,8 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-environ.Env.read_env(os.path.join(BASE_DIR / '././.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR / '././.env'))
+environ.Env.read_env(BASE_DIR.parent.joinpath('.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
