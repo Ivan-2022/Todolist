5c2a98762de039fdd8edad1ca5c488726a59813a
352 <ivan-2022>
2022-12-03 13:23:54 +0300

    add get_env_variable 2

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -38,7 +38,7 @@ def get_env_variable(var_name):
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable("${SECRET_KEY}")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
