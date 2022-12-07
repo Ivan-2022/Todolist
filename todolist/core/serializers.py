a601315e4598639c129de07f06acfdf3339a39d5
352 <ivan-2022>
2022-12-07 18:08:39 +0300

    add login

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from .models import User
 
 
@@ -22,3 +23,21 @@ class UserCreateSerializer(serializers.ModelSerializer):
         del validated_data['password_repeat']
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email", "password")
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise AuthenticationFailed
+        return user
--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,7 +1,20 @@
-from rest_framework import generics
+from django.contrib.auth import login
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from .serializers import UserCreateSerializer
+from .serializers import UserCreateSerializer, LoginSerializer
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=self.request, user=user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
