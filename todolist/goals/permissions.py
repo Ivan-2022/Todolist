aa6614c635168411a818b36099a9b636b0e18819
352 <ivan-2022>
2022-12-16 19:46:09 +0300

    update commentpermission

--- todolist/goals/permissions.py
+++ todolist/goals/permissions.py
@@ -48,8 +48,8 @@ class CommentPermissions(permissions.BasePermission):
             return False
         if request.method in permissions.SAFE_METHODS:
             return BoardParticipant.objects.filter(
-                user=request.user, board=obj.goal.category.board
+                user=request.user, board=obj.category.board
             ).exists()
         return BoardParticipant.objects.filter(
-            user=request.user, board=obj.goal.category.board, role__in=[BoardParticipant.Role.owner,
+            user=request.user, board=obj.category.board, role__in=[BoardParticipant.Role.owner,
                                                                   BoardParticipant.Role.writer]).exists()
