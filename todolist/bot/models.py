90ca4424d2b1a5ba75b0a187e4822b2e9009d1fe
352 <ivan-2022>
2022-12-29 19:01:32 +0300

    исправил

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -5,7 +5,7 @@ from core.models import User
 
 class TgUser(models.Model):
     tg_chat_id = models.PositiveBigIntegerField()
-    tg_user_id = models.PositiveBigIntegerField(unique=True)
+    # tg_user_id = models.PositiveBigIntegerField(unique=True)
     username = models.CharField(max_length=100, null=True, blank=True)
     user = models.ForeignKey('core.User', on_delete=models.PROTECT, null=True)
     verification_code = models.CharField(max_length=50, verbose_name="код подтверждения")
