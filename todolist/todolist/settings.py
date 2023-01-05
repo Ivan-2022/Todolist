7b45dabcbc9c684ea4a9b7ad4f8891f7b16856bc
352 <ivan-2022>
2023-01-05 18:54:37 +0300

    update client.py9

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -21,7 +21,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-environ.Env.read_env(os.path.join(BASE_DIR / '././.env'))
+environ.Env.read_env(os.path.join(BASE_DIR / '../.env'))
 
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
